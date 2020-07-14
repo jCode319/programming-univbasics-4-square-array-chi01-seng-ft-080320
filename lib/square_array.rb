eed63a7cff74ca8383e705b8b246e2fc7557bc4e
Jarrell Jackson <jdjack3@gmail.com>
2020-07-14 19:36:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,8 @@ def square_array(array)
   new_array = []
   counter = 0
   while counter < array.length do
-      array[counter] ** 2
-      num.push(array)
+      new_array << (array[counter] ** 2)
+      counter += 1
   end
   new_array
 end
